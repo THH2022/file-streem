9f4caf33b95f6c7adf737cd05698a575479057ac
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-14 19:48:24 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,8 +18,8 @@ class Var(object):
     name = str(getenv('name', 'THH_Streem_Bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '0'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002194656673'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002194656673'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002111303673'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002111303673'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
