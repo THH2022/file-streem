d19ed807ae837e67f21f44ad8c3987e3f2bcff8b
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-07-14 13:55:26 +0530

    Update stream_routes.py

--- biisal/server/stream_routes.py
+++ biisal/server/stream_routes.py
@@ -25,17 +25,8 @@ routes = web.RouteTableDef()
 async def root_route_handler(_):
     return web.json_response(
         {
-            "server_status": "running",
             "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
-            "connected_bots": len(multi_clients),
-            "loads": dict(
-                ("bot" + str(c + 1), l)
-                for c, (_, l) in enumerate(
-                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
-                )
-            ),
-            "version": __version__,
+            "Bot Sahi Se Chal Raha He...."
         }
     )
 
